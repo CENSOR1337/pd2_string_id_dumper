33e8b85fb490b69fee9fab2b23b53fa6a5a22eb2
DESKTOP-E5RE6S8\Metee_Laptop <viixxivvimm@gmail.com>
2020-12-09 14:46:08 +0700

    - added hashlist dump - prevent some crash

--- localizationmanager.lua
+++ localizationmanager.lua
@@ -1,37 +1,58 @@
 local text_original = LocalizationManager.text
-local data = {}
+local data = nil
+local destination_path = SavePath .. "/string_id_dumper_output/"
 
-local destination_path = SavePath .. "/string_id_dumper_output/raw/"
+local DumpHashlist = true
+local DrumpStringID_and_String = false
 
-function _update_string_data()
-    local file = io.open(destination_path .. "data_string_ID.json", "r")
+function ReadJsonFile(file_path, open_mode)
+    local file = io.open(file_path, open_mode)
     if file then
-        data = json.decode(file:read("*all"))
+        local JsonSaveData = json.decode(file:read("*all"))
         file:close()
+        if (JsonSaveData) then
+            return JsonSaveData
+        else
+            return {}
+        end
+    else
+        return {}
     end
 end
-_update_string_data()
+
+data = ReadJsonFile(destination_path .. "/raw/" .. "data_string_ID.json", "r")
 
 function LocalizationManager:text(string_id, ...)
-    if (string_id) then
-        if not (data[string_id]) then
-            local str = text_original(self, string_id, ...)
-            if not str:match("ERROR") then
-                if (data) and string.len(str) > 0 then
-					data[string_id] = str
-                    local file = io.open(destination_path .. "data_string_ID.json", "w")
-                    if file then
-                        file:write(json.encode(data))
+    if (not (string.is_nil_or_empty(string_id))) then
+        local str = text_original(self, string_id, ...)
+        if not str:match("ERROR") then
+            -- Dump String id and string
+            if (DrumpStringID_and_String) then
+                if not (data[string_id]) then
+                    if (data) and (not (string.is_nil_or_empty(str))) then
+                        data[string_id] = str
+                        if data[string_id] then
+                            local file = io.open(destination_path .. "/raw/" .. "data_string_ID.json", "w")
+                            if file then
+                                file:write(json.encode(data))
+                                file:close()
+                            end
+                        end
+                    end
+                end
+            end
+            -- Dump Hashlist
+            if (DumpHashlist) then
+                local file = io.open(destination_path .. "/raw/" .. "hashlist", "r+")
+                if file then
+                    local contents = file:read("*all")
+                    if not (contents:find(string_id)) then
+                        file:write(string_id .. "\n")
                         file:close()
                     end
                 end
             end
         end
     end
-    return true and text_original(self, string_id, ...)
-end
-
-function LocalizationManager:ids(file)
-    log(Localizer:ids(Idstring(file)))
-    return Localizer:ids(Idstring(file))
+    return text_original(self, string_id, ...)
 end
